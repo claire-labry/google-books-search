3de961f530b180700285f52b6697937db9f4872d
claire-labry <clairestjlabry@gmail.com>
2020-06-08 17:03:31 -0500

    adding more starting files

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serves static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+// API routes
+
+// Send every other request to the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port:${PORT}`);
+});
