c4e71076688561de2aa5f0d23189bd8970f825e8
claire-labry <clairestjlabry@gmail.com>
2020-06-11 17:08:05 -0500

    added search page and created utils file api.js

---
+++ client/src/utils/API.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export default {
+  // api to search books from google books
+  searchBook: (query) => {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+  },
+
+  getBooks: () => {
+    return axios.get('api/books');
+  },
+
+  saveBook: (data) => {
+    return axios.post('/api/books', data);
+  },
+
+  deleteBook: (id) => {
+    return axios.delete(`/api/books/${id}`);
+  },
+
+  updateBook: (id, status) => {
+    return axios.put(`/api/books/${id}`, status);
+  },
+};
