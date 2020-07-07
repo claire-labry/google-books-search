4cee80673a9037ddd98d0af269c8bb07c994971e
claire-labry <clairestjlabry@gmail.com>
2020-07-07 13:03:01 -0500

    added save component

---
+++ client/src/components/Save/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+const Save= (props) =>{
+
+  return(
+    <div>
+      {props.data.map((saved)=>{
+        return(
+          <div key={saved._id}>
+            <div>
+              <div id='pic'>
+                <img src={saved.image} alt={saved.title} />
+                <p>{saved.title}</p>
+                <p>by:{saved.author}</p>
+                <div>
+                  <button type='button' id={book.id} onClick={() => props.deleteBook(saved._id)}> Delete</button>
+                </div>
+                <div>
+                  <a href={saved.link} target='_blank'>
+                    <button type='button'>More Info</button>
+                  </a>
+                </div>
+              </div>
+              <div>{saved.description}</div>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Save
