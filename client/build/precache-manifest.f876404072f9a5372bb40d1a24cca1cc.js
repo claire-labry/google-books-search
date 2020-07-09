15981930af9eda554106095128b16bab7d232bd7
claire-labry <clairestjlabry@gmail.com>
2020-07-09 15:42:39 -0500

    adding build files

---
+++ client/build/precache-manifest.f876404072f9a5372bb40d1a24cca1cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93396dea189db976ef1576ccc093b12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c73152a915df18c5397",
+    "url": "/static/css/main.f34ae9e6.chunk.css"
+  },
+  {
+    "revision": "5aeaf3aee73f87fdc32d",
+    "url": "/static/js/2.dcecde80.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dcecde80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c73152a915df18c5397",
+    "url": "/static/js/main.c62ebf39.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
